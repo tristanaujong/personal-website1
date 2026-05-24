b9b250a6b7d3523dbabf17814ec8bee417332ec1
tristanaujong <tristanaujong@gmail.com>
2026-05-24 01:26:15 -0500

    header

---
+++ my-app/constants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+    {id: 'home', title: 'Home'},
+    {id: 'projects', title: 'Projects'},
+    {id: 'experience', title: 'Experience'},
+    {id: 'fun', title: 'Fun Stuff'},
+
+]
